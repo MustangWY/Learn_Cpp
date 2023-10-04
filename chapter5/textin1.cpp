ec4c8bf666a8e924885e0923a63f580cbac0f001
MustangWY <2681216465@qq.com>
2023-10-04 17:20:47 +0800

    10.4

--- chapter5/textin1.cpp
+++ chapter5/textin1.cpp
@@ -7,7 +7,7 @@ int main (void) {
     cin.get(ch);
     int count = 0;
     //while (ch!='#')
-    while(cin.fail() == false)//eof检测，ctrl+z视为eof，fail函数为真，否则fail（）函数为假
+    while(cin.fail() == false)//eof检测，ctrl+z视为eof，fail函数为真，否则fail（）函数为假 ctrl+z模拟eof
     {
         cout<<ch;
         count++;
@@ -15,7 +15,10 @@ int main (void) {
         //cin>>ch;//cin会跨过空格和空白字符和换行字符
         /* code */
     }
+    cin.clear();//清楚eof标记
     cout<<endl<<count<<" characters read ";
     
     return 0;
 }
+//最精简：
+//while(cin.get(ch)){}
