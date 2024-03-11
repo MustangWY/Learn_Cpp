e511a988a097e44c5cacb9126cdcc26ace652540
MustangWY <2681216465@qq.com>
2024-03-11 21:30:07 +0800

    3.11

--- cpp_multiple/DataStruct/List/List.h
+++ cpp_multiple/DataStruct/List/List.h
@@ -23,37 +23,16 @@ public:
     int ListDelete_Sq(Sqlist &L, int i, ElemType &e);
     int IsEqual(ElemType, ElemType);
     void UnionList_Sq(Sqlist &La, Sqlist &&Lb);
-    void ShowList(const Sqlist &L);
+    void ShowList();
     int push_back(ElemType);
-    int push_front(Sqlist &l, ElemType e);
-    int pop_back(Sqlist &l, ElemType &e);
-    int pop_front(Sqlist &l, ElemType &e);
+    int push_front(ElemType e);
+    int pop_back(ElemType &e);
+    int pop_front(ElemType &e);
     int find(Sqlist &l, ElemType e);
     int Show_Length(Sqlist);
     ~Sqlist();
 
 };
-
-
-
-
-
-
-
-void MergeList_Sq(Sqlist L1, Sqlist L2, Sqlist &L3);
-int LocateElem_Sq(Sqlist L, ElemType e, int (*compare) (ElemType, ElemType));
-int InitList_SQ(Sqlist &L);
-int ListInsert_Sq(Sqlist &l, int i, ElemType e);
-int ListDelete_Sq(Sqlist &L, int i, ElemType &e);
-int IsEqual(ElemType, ElemType);
-void UnionList_Sq(Sqlist &La, Sqlist &&Lb);
-void ShowList(const Sqlist &L);
-int push_back(Sqlist& ,ElemType);
-int push_front(Sqlist &l, ElemType e);
-int pop_back(Sqlist &l, ElemType &e);
-int pop_front(Sqlist &l, ElemType &e);
-int find(Sqlist &l, ElemType e);
-int Show_Length(Sqlist);
 
 
 #endif
